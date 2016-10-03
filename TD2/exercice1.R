b1443a4f99da59ed1ba7f1f536ea93476bce728c
LolaVincent <lolascuba@orange.fr>
2016-10-03 12:27:05 +0200

    suite exo1

--- TD2/exercice1.R
+++ TD2/exercice1.R
@@ -1,4 +1,10 @@
 data <- read.table("prostate.data")
 reg.lpsa <- lm(data$lpsa~data$lcavol+data$lweight+data$age+data$lbph+data$svi+data$lcp+data$gleason+data$pgg45)
-#Significativement non nuls : lcavol, svi
-confint(reg.lpsa) # Bornes de l'intervalle de confiance 
+#Significativement non nuls : lcavol, svi, weight(?)
+
+inter <- confint(reg.lpsa) # Bornes de l'intervalle de confiance 
+
+plot(reg.lpsa$fitted.values, data$lpsa) # f(x0) correspond au fitted.values de reg.lpsa ?
+plot(data$lpsa,reg.lpsa$residuals) # résidus bruts en fonction des yi
+plot(data$lpsa, rstudent(reg.lpsa)) # résidus standardisés en fonction des yi
+plot( reg.lpsa$fitted.values, reg.lpsa$residual) # vérification de la normalité des résidus, ok
