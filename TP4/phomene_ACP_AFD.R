4dae2a303e5e5fb1ea6e3772b29f1151a49848c9
Anaig Marechal <anaig.marechal@etu.utc.fr>
2016-11-14 16:46:38 +0100

    knn sur AFD

--- TP4/phomene_ACP_AFD.R
+++ TP4/phomene_ACP_AFD.R
@@ -32,60 +32,70 @@ plot(acp.train$scores[,c(2,3)], pch=16, col=palette()[phoneme.train$g])
 abline(h=0, v=0) #3 groupes qui se distinguent
 legend("topright", inset=.05, title="Phoneme", c("aa", "ao", "dcl","iy","sh"), fill=palette(), horiz=FALSE)
 
-
-#knn avant ACP  
-#m <- matrix(nrow=10, ncol=3)
-#for(k in 1:10) {
-  #for(i in 1:3) {
-    #phoneme.knn <- knn(train = phoneme.trainquant, test = phoneme.testquant, cl = phoneme.trainclass, k=k)
-    #perf.knn <- table(phoneme.testclass, phoneme.knn) 
-    #Il est intéressant de voir en affichant perf.knn que tous les >0 hors diag concernent aa et ao
-    #m[k,i]<-(sum(perf.knn)-sum(diag(perf.knn)))/ntest #9%
-  #}
-#}
-#rowMeans(m) #Si on veut comparer en fonction de k. Je n'ai mis que jusqu'à 3 parce que ça prend bcp de tps
-#boxplot(t(m)) #k=9 intéressant
-
 #knn après ACP 
 phoneme.knn.acp <- knn(train = acp.train$scores, test = acp.test$scores, cl = phoneme.trainclass, k=5)
-phoneme.knn.acp 
 perf.knn.acp <- table(phoneme.test[,258], phoneme.knn.acp)
-(sum(perf.knn.acp)-sum(diag(perf.knn.acp)))/ntest #15%
+(sum(perf.knn.acp)-sum(diag(perf.knn.acp)))/ntest #15%  
+
 #ACP permet de supprimer le bruit 
 #puis AFD sélectionne les var les + discriminantes possibles parmi elles
 
+
+
+
 #AFD seule : 
 library(MASS)
-phonfactor <- as.factor(phoneme$g)
-lda.phoneme<-lda(phoneme$g~. ,data=phoneme_quant)
+lda.phoneme<-lda(phoneme.train[,258]~. ,data=phoneme.trainquant)
 U <- lda.phoneme$scaling
-X <- as.matrix(phoneme_quant)
+X <- as.matrix(phoneme.trainquant)
 Z <- X%*%U
+
+lda.phonemetest<-lda(phoneme.test[,258]~. ,data=phoneme.testquant)
+Utest <- lda.phonemetest$scaling
+Xtest <- as.matrix(phoneme.testquant)
+Ztest <- Xtest%*%Utest
 
 #Composantes de l'AFD à utiliser : 
 cp1 <- 1
 cp2 <- 2
 
-plot(Z[phoneme$g=="aa",cp1],Z[phoneme$g=="aa",cp2],xlim=range(Z[,1]),ylim=range(Z[,2]),xlab="Z1",ylab="Z2")
-points(Z[phoneme$g=="ao",cp1],Z[phoneme$g=="ao",cp2],pch=2,col="blue")
-points(Z[phoneme$g=="dcl",cp1],Z[phoneme$g=="dcl",cp2],pch=3,col="red")
-points(Z[phoneme$g=="iy",cp1],Z[phoneme$g=="iy",cp2],pch=4,col="pink")
-points(Z[phoneme$g=="sh",cp1],Z[phoneme$g=="sh",cp2],pch=5,col="yellow")
+plot(Z[phoneme.train[,258]=="aa",cp1],Z[phoneme.train[,258]=="aa",cp2],xlim=range(Z[,1]),ylim=range(Z[,2]),xlab="Z1",ylab="Z2")
+points(Z[phoneme.train[,258]=="ao",cp1],Z[phoneme.train[,258]=="ao",cp2],pch=2,col="blue")
+points(Z[phoneme.train[,258]=="dcl",cp1],Z[phoneme.train[,258]=="dcl",cp2],pch=3,col="red")
+points(Z[phoneme.train[,258]=="iy",cp1],Z[phoneme.train[,258]=="iy",cp2],pch=4,col="pink")
+points(Z[phoneme.train[,258]=="sh",cp1],Z[phoneme.train[,258]=="sh",cp2],pch=5,col="yellow")
 legend("topleft", inset=.05, title="Phoneme", c("aa", "ao", "dcl","iy","sh"), fill=c("black","blue","red","pink","yellow"), horiz=TRUE)
 
+#knn après AFD
+phoneme.knn.afd <- knn(train = Z, test = Ztest, cl = phoneme.trainclass, k=9)
+perf.knn.afd <- table(phoneme.test[,258], phoneme.knn.afd)
+(sum(perf.knn.afd)-sum(diag(perf.knn.afd)))/ntest #4.5%  
+
+
+
 #AFD en plus de l'ACP
-lda.phoneme2<-lda(phoneme$g~. ,data=as.data.frame(acp$scores[,1:5]))
+lda.phoneme2<-lda(phoneme.train[,258]~. ,data=as.data.frame(acp.train$scores[,1:5]))
 U2 <- lda.phoneme2$scaling
-X2 <- as.matrix(acp$scores[,1:5])
+X2 <- as.matrix(acp.train$scores[,1:5])
 Z2 <- X2%*%U2
 
-plot(Z2[phoneme$g=="aa",cp1],Z2[phoneme$g=="aa",cp2],xlim=range(Z2[,1]),ylim=range(Z2[,2]),xlab="Z1",ylab="Z2")
-points(Z2[phoneme$g=="ao",cp1],Z2[phoneme$g=="ao",cp2],pch=2,col="blue")
-points(Z2[phoneme$g=="dcl",cp1],Z2[phoneme$g=="dcl",cp2],pch=3,col="red")
-points(Z2[phoneme$g=="iy",cp1],Z2[phoneme$g=="iy",cp2],pch=4,col="pink")
-points(Z2[phoneme$g=="sh",cp1],Z2[phoneme$g=="sh",cp2],pch=5,col="yellow")
-legend("topleft", inset=.05, title="Phoneme", c("aa", "ao", "dcl","iy","sh"), fill=c("black","blue","red","pink","yellow"), horiz=TRUE)       
+lda.phonemetest2<-lda(phoneme.test[,258]~. ,data=as.data.frame(acp.test$scores[,1:5]))
+Utest2 <- lda.phonemetest2$scaling
+Xtest2 <- as.matrix(acp.test$scores[,1:5])
+Ztest2 <- Xtest2%*%Utest2
 
-#Avec ACP + AFD on a une meilleure séparation des phonemes dcl et iy
-# Pour sélectionner la meilleure méthode de réducteur de dimensions, on peut regarder le taux d'erreur des Kppv
+plot(Z2[phoneme.train[,258]=="aa",cp1],Z2[phoneme.train[,258]=="aa",cp2],xlim=range(Z2[,1]),ylim=range(Z2[,2]),xlab="Z1",ylab="Z2")
+points(Z2[phoneme.train[,258]=="ao",cp1],Z2[phoneme.train[,258]=="ao",cp2],pch=2,col="blue")
+points(Z2[phoneme.train[,258]=="dcl",cp1],Z2[phoneme.train[,258]=="dcl",cp2],pch=3,col="red")
+points(Z2[phoneme.train[,258]=="iy",cp1],Z2[phoneme.train[,258]=="iy",cp2],pch=4,col="pink")
+points(Z2[phoneme.train[,258]=="sh",cp1],Z2[phoneme.train[,258]=="sh",cp2],pch=5,col="yellow")
+legend("topleft", inset=.05, title="Phoneme", c("aa", "ao", "dcl","iy","sh"), fill=c("black","blue","red","pink","yellow"), horiz=TRUE) 
+
+#knn après AFD/ACP
+phoneme.knn.afd2 <- knn(train = Z2, test = Ztest2, cl = phoneme.trainclass, k=9)
+perf.knn.afd2 <- table(phoneme.test[,258], phoneme.knn.afd2)
+(sum(perf.knn.afd2)-sum(diag(perf.knn.afd2)))/ntest #70%....  
+
+#La meilleure méthode a adopter au vu des résultats sur KNN est l'AFD seule 
+#car c'est elle qui offre une meilleure séparation des clusters
 
