040e4bdd3785b5f76882af73704076cf02ebca8a
Anaig Marechal <anaig.marechal@etu.utc.fr>
2016-11-21 11:39:58 +0100

    best subset selection

--- TP4/Breast_Cancer/mlr.R
+++ TP4/Breast_Cancer/mlr.R
@@ -13,3 +13,31 @@ plot(residuals(lm.bc), fitted(lm.bc)) #Pas de relation entre l'erreur et les Xi 
 
 h <- predict(lm.bc, testdata, interval="confidence")
 summary(h)
+
+#Trop de prédicteurs pour pouvoir faire un choix, rendre la relation non-linéaire, etc
+#--> subset selection
+library(leaps)
+reg.fit <- regsubsets(as.matrix(train[33])~., data=traindata, method='exhaustive', nvmax=32)
+plot(reg.fit, scale='r2')
+reg.summary <- summary(reg.fit)
+reg.summary$rsq
+plot(reg.summary$adjr2, xlab="Number of Variables", ylab="Adjusted RSq", type="l") #RSS en fonction de R2 ajusté
+which.max(reg.summary$adjr2) #16 --> model avec le plus grand R2
+points(16, reg.summary$adjr2[16], col="red", cex=2, pch=20)
+#On cherche le plus grand R2 ou le plus faible RSS
+#method='exhaustive' --> best subset selection
+#method='forward' --> stepwise selection
+#method='backward' --> stepwise selection
+
+#test avec 16 : 
+lm.bc = lm(as.matrix(train[33])~texture_mean+area_mean+compactness_mean+concave_points_mean+symmetry_mean+ radius_se+ texture_se+ perimeter_se+smoothness_se+compactness_se+concavity_se+texture_worst+perimeter_worst+area_worst+smoothness_worst+fractal_dimension_worst, data=traindata)
+summary(lm.bc) # RSE=32%
+plot(fitted(lm.bc), as.matrix(train[33]))  #valeurs prédites yhat en fonction des yi 
+#--> pas de relation visible sur ce graphique
+
+reg.fwd <- regsubsets(as.matrix(train[33])~., data=traindata, method='forward', nvmax=32)
+plot(reg.fwd, scale='r2')
+fwd.summary <- summary(reg.fwd)
+plot(fwd.summary$adjr2, xlab="Number of Variables", ylab="Adjusted RSq", type="l") #RSS en fonction de R2 ajusté
+which.max(fwd.summary$adjr2) #18 --> model avec le plus grand R2
+points(18, fwd.summary$adjr2[18], col="red", cex=2, pch=20)
