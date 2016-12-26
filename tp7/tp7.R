ed9d7eb7d7f333196579e79c3c61ba1c5a6ec904
LolaVincent <lolascuba@orange.fr>
2016-12-26 10:16:17 +0100

    premier TD

---
+++ tp7/tp7.R
@@ -0,0 +1,21 @@
+data <- load("data_expressions.RData")
+X # données
+Y # classification
+X[i] # 
+
+
+I<-matrix(X[i,],60,70)
+I1 <- apply(I, 1, rev)
+image(t(I1),col=gray(0:255 / 255))
+
+
+data_not_null <- matrix(nrow=216)
+for (i in 1:4200){ if (X[,i]!=0){ data_not_null <- cbind(data_not_null, X[,i]) }}
+data_not_null <- data_not_null[,2:ncol(data_not_null)]
+
+
+data_acp <- prcomp(data_not_null) # scale =TRUE données normées, pas princomp trop de variables par rapport au nombre d'individus
+#package factoextra pour la foncion get_pca_var
+
+
+pairs(data_acp$x[,1:5], col=palette()[y])
